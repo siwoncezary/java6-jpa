134689dcee33811a43abdf3054548bc6360a9e3b
cs <siwon.cezary@gmail.com>
2020-08-23 14:14:12 +0200

    init

--- src/main/java/EntityDemo.java
+++ src/main/java/EntityDemo.java
@@ -18,6 +18,8 @@ public class EntityDemo {
         System.out.println("2. Wyświetl wszystkie encje");
         System.out.println("3. Edytuj encję");
         System.out.println("4. Usuń encję");
+        System.out.println("5. Odłączanie encji");
+        System.out.println("6. Scalanie encji");
         System.out.println("0. Wyjście");
         while (!scanner.hasNextInt()) {
             System.out.println("Wpisz numer polecenia!!!");
@@ -62,6 +64,54 @@ public class EntityDemo {
         em.getTransaction().commit();
     }
 
+    static void editEntity(){
+        System.out.println("Podaj id edytowanej encji:");
+        long id = scanner.nextLong();
+        scanner.nextLine();
+
+        em.getTransaction().begin();
+        //entity jest zarządzane przez managera i zmiany w obiekcie są utrwalane w bazie
+        Alcohol entity = em.find(Alcohol.class, id);
+        System.out.println(entity);
+        System.out.println("Podaj nową nazwę:");
+        String name = scanner.nextLine();
+        System.out.println("Podaj nową pojemność:");
+        float capacity = scanner.nextFloat();
+        System.out.println("Podaj nową zawartość:");
+        float voltage = scanner.nextFloat();
+        entity.setName(name);
+        entity.setVoltage(new BigDecimal(voltage));
+        entity.setCapacity(new BigDecimal(capacity));
+        //tu się kończy transakcja i zarządzanie obiektem przez managera
+        em.getTransaction().commit();
+    }
+
+    static void detachEntity(){
+        System.out.println("Podaj id odłączanej encji:");
+        long id = scanner.nextLong();
+        Alcohol entity = em.find(Alcohol.class, id);
+        em.detach(entity);
+        em.getTransaction().begin();
+        entity.setName("");
+        em.getTransaction().commit();
+        System.out.println(entity);
+    }
+
+    static void mergeEntity(){
+        System.out.println("Podaj id scalanej encji:");
+        long id = scanner.nextLong();
+        scanner.nextLine();
+        Alcohol entity = new Alcohol();
+        entity.setId(id);
+        System.out.println("Podaj nazwę:");
+        String name = scanner.nextLine();
+        entity.setName(name);
+        em.getTransaction().begin();
+        Alcohol newEntity = em.merge(entity);
+        em.getTransaction().commit();
+        System.out.println(newEntity);
+    }
+
     public static void main(String[] args) {
         while (true) {
             switch (menu()) {
@@ -72,9 +122,16 @@ public class EntityDemo {
                     printAllEntities();
                     break;
                 case 3:
+                    editEntity();
                     break;
                 case 4:
                     deleteEntity();
+                    break;
+                case 5:
+                    detachEntity();
+                    break;
+                case 6:
+                    mergeEntity();
                     break;
                 case 0:
                     return;
